d1684bcf693a2088539a36026dddf28a19370dde
yurik1776 <yurik1776@gmail.com>
2017-08-06 20:50:39 +0300

    replaced classes for concatination inheritance

--- build/backend.js
+++ build/backend.js
@@ -74,253 +74,52 @@ module.exports = __webpack_require__(1);
 
 /***/ }),
 /* 1 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+/***/ (function(module, __webpack_exports__) {
 
 "use strict";
-Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__index__ = __webpack_require__(2);
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+var _this = this;
 
-var _class, _class2;
+var Emit = function Emit(_ref) {
+  var listeners = _ref.listeners;
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-
-
-var main = Object(__WEBPACK_IMPORTED_MODULE_0__index__["a" /* createObservable */])();
-var observer = Object(__WEBPACK_IMPORTED_MODULE_0__index__["b" /* createObserver */])('listener1', main);
-observer.subscribe('go', function (data) {
-  console.log(data);
-});
-var extend = Object(__WEBPACK_IMPORTED_MODULE_0__index__["c" /* decorateObservable */])(main, false);
-
-var Car = extend(_class = function () {
-  function Car(wheels) {
-    _classCallCheck(this, Car);
-
-    this.wheels = wheels;
-  }
-
-  _createClass(Car, [{
-    key: 'go',
-    value: function go() {
-      console.log('going');
-    }
-  }, {
-    key: 'stop',
-    value: function stop() {
-      console.log('stopping');
-    }
-  }]);
-
-  return Car;
-}()) || _class;
-
-var Truck = extend(_class2 = function Truck(wheels) {
-  _classCallCheck(this, Truck);
-}) || _class2;
-
-var car = new Car(4);
-var truck = new Truck(6);
-var observer2 = Object(__WEBPACK_IMPORTED_MODULE_0__index__["b" /* createObserver */])('tr listener', truck);
-var observer3 = Object(__WEBPACK_IMPORTED_MODULE_0__index__["b" /* createObserver */])('truck listener', truck);
-observer2.subscribe('go', function (data) {
-  console.log(data);
-});
-observer3.subscribe('go', function (data) {
-  console.log(data);
-});
-// car.emit('go', 'car going')
-truck.emit('go', 'truck started driving');
-// truck.go()
-
-/***/ }),
-/* 2 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return createObservable; });
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "b", function() { return createObserver; });
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "c", function() { return decorateObservable; });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__observer__ = __webpack_require__(3);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__observable__ = __webpack_require__(4);
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-
-
-
-// creates a new observable
-function createObservable() {
-  return new __WEBPACK_IMPORTED_MODULE_1__observable__["a" /* default */]();
-}
-
-// creates a new observer
-function createObserver(name, parent) {
-  return new __WEBPACK_IMPORTED_MODULE_0__observer__["a" /* default */](name, parent);
-}
-
-// TODO make tests...
-// TODO test decorator and rewrite for better understanding and use cases
-function decorateObservable(parentObservable) {
-  var passListeners = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : false;
-
-  // if class, then type is func, if instance, type is object
-  // if class and parent observable has listeners on it, pass them to child
-  var listenersToPass = void 0;
-  if (typeof parentObservable === 'function') {
-    listenersToPass = parentObservable.prototype.listeners || {};
-  } else if ((typeof parentObservable === 'undefined' ? 'undefined' : _typeof(parentObservable)) === 'object') {
-    var fn = function fn() {};
-
-    listenersToPass = Object.create(parentObservable.listeners);
-
-    fn.prototype = parentObservable.__proto__;
-    parentObservable = fn;
-  }
-  // handle merging prototypes of created class with decorated class and pass listeners from parent if flag === true
-  return function (ObservableClass) {
-    var passedClassMethods = Object.getOwnPropertyNames(ObservableClass.prototype).slice(1);
-
-    var Observabled = function (_parentObservable) {
-      _inherits(Observabled, _parentObservable);
-
-      function Observabled() {
-        _classCallCheck(this, Observabled);
-
-        var _this = _possibleConstructorReturn(this, (Observabled.__proto__ || Object.getPrototypeOf(Observabled)).call(this));
-
-        _this.listeners = passListeners ? listenersToPass : {};
-        return _this;
-      }
-
-      return Observabled;
-    }(parentObservable);
-
-    Observabled.prototype.listeners = listenersToPass;
-    passedClassMethods.forEach(function (method) {
-      return Observabled.prototype[method] = ObservableClass.prototype[method];
-    });
-    return Observabled;
-  };
-}
-
-
-
-/***/ }),
-/* 3 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return Observer; });
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-// observer class
-var Observer = function () {
-  function Observer(name, parentEmitter) {
-    _classCallCheck(this, Observer);
-
-    this.name = name;
-    this.parentEmitter = parentEmitter;
-  }
-  // remove listener on a specific event, this.name is passed an will be compared to fn.name which is set on subscription
-
-
-  _createClass(Observer, [{
-    key: 'unsubscribe',
-    value: function unsubscribe(label) {
-      this.parentEmitter.removeListener(label, this.name);
-    }
-    // add a listener on a specific event, add this.name on fn.name
-
-  }, {
-    key: 'subscribe',
-    value: function subscribe(label, fn) {
-      // TODO check if arrow func and replace for regular func in order to have bind this with name and parent emitter...
-      Object.defineProperty(fn, 'name', {
-        enumerable: false,
-        configurable: true,
-        writable: true,
-        value: this.name
-      });
-      // bind fn to this
-      this.parentEmitter.addListener(label, fn.bind(this));
-    }
-    // emit change to parent
-
-  }, {
-    key: 'emit',
-    value: function emit(label) {
-      var _parentEmitter;
-
+  var ls = listeners;
+  return {
+    emit: function emit(label) {
       for (var _len = arguments.length, args = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
         args[_key - 1] = arguments[_key];
       }
 
-      (_parentEmitter = this.parentEmitter).childNotify.apply(_parentEmitter, [label].concat(args));
+      var listeners = ls[label];
+      if (listeners && listeners.length) {
+        listeners.forEach(function (listener) {
+          return listener.apply(undefined, args);
+        });
+        return true;
+      }
+      return false;
     }
-  }]);
+  };
+};
 
-  return Observer;
-}();
+var AddListener = function AddListener(_ref2) {
+  var listeners = _ref2.listeners;
 
-
-
-/***/ }),
-/* 4 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return Observable; });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_events__ = __webpack_require__(5);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_events___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_events__);
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-
-
-// observable class
-
-var Observable = function (_EventEmitter) {
-  _inherits(Observable, _EventEmitter);
-
-  function Observable() {
-    _classCallCheck(this, Observable);
-
-    // all listeners
-    var _this = _possibleConstructorReturn(this, (Observable.__proto__ || Object.getPrototypeOf(Observable)).call(this));
-
-    _this.listeners = {};
-    return _this;
-  }
-  // adding the event name as key and all listeners as an array value
-
-
-  _createClass(Observable, [{
-    key: 'addListener',
-    value: function addListener(label, callback) {
-      if (!this.listeners[label]) this.listeners[label] = [];
-      this.listeners[label].push(callback);
+  return {
+    addListener: function addListener(label, callback) {
+      if (!listeners[label]) listeners[label] = [];
+      listeners[label].push(callback);
     }
-    // remove listener by observer name, when adding a listener, fn.name is added the observer name for easier removal
+  };
+};
 
-  }, {
-    key: 'removeListener',
-    value: function removeListener(label, observerName) {
-      var listeners = this.listeners;
+var RemoveListener = function RemoveListener(_ref3) {
+  var listeners = _ref3.listeners;
+
+  var ls = listeners;
+  return {
+    removeListener: function removeListener(label, observerName) {
       var index = null;
+      var listeners = ls;
       if (listeners[label] && listeners[label].length) {
         for (var i = 0, len = listeners[label].length; i < len; i++) {
           if (listeners[label][i].name.includes(observerName)) {
@@ -335,48 +134,90 @@ var Observable = function (_EventEmitter) {
         }
       }
     }
-    // emit to all observers subscribed to specific event, call all fns and pass args to them
+  };
+};
 
-  }, {
-    key: 'emit',
-    value: function emit(label) {
-      for (var _len = arguments.length, args = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
-        args[_key - 1] = arguments[_key];
-      }
+var ChildNotify = function ChildNotify(_ref4) {
+  var emit = _ref4.emit;
 
-      var listeners = this.listeners[label];
-      if (listeners && listeners.length) {
-        listeners.forEach(function (listener) {
-          return listener.apply(undefined, args);
-        });
-        return true;
-      }
-      return false;
-    }
-    // when a listener emits a change, notify all children
-
-  }, {
-    key: 'childNotify',
-    value: function childNotify(label) {
+  var em = emit;
+  return {
+    childNotify: function childNotify(label) {
       for (var _len2 = arguments.length, args = Array(_len2 > 1 ? _len2 - 1 : 0), _key2 = 1; _key2 < _len2; _key2++) {
         args[_key2 - 1] = arguments[_key2];
       }
 
       // same as emit but fired from child to notify parent that notifies all children
-      return this.emit.apply(this, [label].concat(args));
+      return em.apply(undefined, [label].concat(args));
     }
-  }]);
+  };
+};
 
-  return Observable;
-}(__WEBPACK_IMPORTED_MODULE_0_events___default.a);
+var AttachListeners = function AttachListeners(_ref5) {
+  var listeners = _ref5.listeners;
 
+  var ls = listeners || {};
+  return {
+    listeners: ls
+  };
+};
 
+var Unsubscribe = function Unsubscribe(_ref6) {
+  var parent = _ref6.parent,
+      name = _ref6.name;
 
-/***/ }),
-/* 5 */
-/***/ (function(module, exports) {
+  var pr = parent;
+  var nm = name;
+  return {
+    unsubscribe: function unsubscribe(label) {
+      pr.removeListener(label, nm);
+    }
+  };
+};
 
-module.exports = require("events");
+var Subscribe = function Subscribe(_ref7) {
+  var parent = _ref7.parent,
+      name = _ref7.name;
+
+  var pr = parent;
+  var nm = name;
+  return {
+    subscribe: function subscribe(label, fn) {
+      Object.defineProperty(fn, 'name', {
+        enumerable: false,
+        configurable: true,
+        writable: true,
+        value: nm
+      });
+      pr.addListener(label, fn.bind(_this));
+    }
+  };
+};
+
+function createObservable() {
+  var listeners = {};
+  return Object.assign({}, Emit({ listeners: listeners }), AddListener({ listeners: listeners }), RemoveListener({ listeners: listeners }), AttachListeners({ listeners: listeners }));
+}
+
+function createObserver(name, parent) {
+  return Object.assign({}, ChildNotify(Emit({ listeners: parent.listeners })), Subscribe({ parent: parent, name: name }), Unsubscribe({ parent: parent, name: name }));
+}
+
+function makeObservable(parent) {
+  // TODO create decorator to make an observable
+}
+
+var main = createObservable();
+var observer = createObserver('observer1', main);
+var observer2 = createObserver('observer2', main);
+observer.subscribe('start', function (data) {
+  return console.log(data);
+});
+observer2.subscribe('start', function (data) {
+  return console.log(data);
+});
+observer.unsubscribe('start');
+main.emit('start', 'awsomeeeee');
 
 /***/ })
 /******/ ]);
--- test.js
+++ test.js
@@ -1,38 +1,119 @@
-import { decorateObservable, createObservable, createObserver } from './index'
+
+
+const Emit = ({listeners}) =>{
+  let ls = listeners
+  return {
+    emit:(label, ...args)=>{
+      let listeners = ls[label]
+      if (listeners && listeners.length) {
+        listeners.forEach(listener => listener(...args))
+        return true
+      }
+      return false
+    }
+  }
+}
+
+const AddListener = ({listeners}) =>{
+  return {
+    addListener:(label, callback)=>{
+      if(!listeners[label]) listeners[label] = []
+      listeners[label].push(callback)
+    }
+  }
+}
+
+const RemoveListener = ({listeners}) =>{
+  let ls = listeners
+  return {
+    removeListener:(label, observerName)=>{
+      let index = null
+      let listeners = ls
+      if(listeners[label] && listeners[label].length){
+        for(let i = 0, len = listeners[label].length; i < len; i++){
+          if(listeners[label][i].name.includes(observerName)){
+            index = i
+            break
+          }
+        }
+        if(index !== null){
+          listeners[label] = listeners[label].filter(fn => fn !== listeners[label][index])
+        }
+      }
+    }
+  }
+}
+
+const ChildNotify = ({emit}) =>{
+  let em = emit
+  return {
+    childNotify:(label, ...args)=>{
+      // same as emit but fired from child to notify parent that notifies all children
+      return em(label, ...args)
+    }
+  }
+}
+
+const AttachListeners = ({listeners}) => {
+  let ls = listeners || {}
+  return {
+    listeners: ls
+  }
+}
+
+const Unsubscribe = ({parent, name}) =>{
+  let pr = parent
+  let nm = name
+  return {
+    unsubscribe: label =>{
+      pr.removeListener(label, nm)
+    }
+  }
+}
+
+const Subscribe = ({parent, name}) =>{
+  let pr = parent
+  let nm = name
+  return {
+    subscribe:(label, fn)=>{
+      Object.defineProperty(fn,'name',{
+        enumerable: false,
+        configurable: true,
+        writable: true,
+        value: nm
+      })
+      pr.addListener(label, fn.bind(this))
+    }
+  }
+}
+
+function createObservable () {
+  let listeners = {}
+  return Object.assign({},
+    Emit({listeners}),
+    AddListener({listeners}),
+    RemoveListener({listeners}),
+    AttachListeners({listeners})
+  )
+}
+
+function createObserver(name, parent){
+  return Object.assign({},
+    ChildNotify(Emit({listeners:parent.listeners})),
+    Subscribe({parent, name}),
+    Unsubscribe({parent, name})
+  )
+}
+
+function makeObservable(parent){
+  // TODO create decorator to make an observable
+}
+
 
 let main = createObservable()
-let observer = createObserver('listener1', main)
-observer.subscribe('go', function(data){console.log(data)})
-let extend = decorateObservable(main, false)
-
-@extend
-class Car{
-  constructor(wheels){
-    this.wheels = wheels
-  }
-  go(){
-    console.log('going')
-  }
-  stop(){
-    console.log('stopping')
-  }
-}
-
-@extend
-class Truck{
-  constructor(wheels){
-
-  }
-}
-
-
-
-let car = new Car(4)
-let truck = new Truck(6)
-let observer2 = createObserver('tr listener', truck)
-let observer3 = createObserver('truck listener', truck)
-observer2.subscribe('go', function(data){console.log(data)})
-observer3.subscribe('go', function(data){console.log(data)})
-// car.emit('go', 'car going')
-truck.emit('go', 'truck started driving')
-// truck.go()
+let observer = createObserver('observer1', main)
+let observer2 = createObserver('observer2', main)
+observer.subscribe('start',data=>console.log(data))
+observer2.subscribe('start',data=>console.log(data))
+observer.unsubscribe('start')
+main.emit('start', 'awsomeeeee')
