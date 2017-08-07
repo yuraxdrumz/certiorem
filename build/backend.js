9fa827d6b2911a52f880937dced09ffe85099d09
yurik1776 <yurik1776@gmail.com>
2017-08-07 18:45:38 +0300

    added decorators

--- build/backend.js
+++ build/backend.js
@@ -76,19 +76,23 @@ module.exports = __webpack_require__(1);
 /* 1 */
 /***/ (function(module, exports) {
 
-var _this = this;
+var _this = this,
+    _dec,
+    _class,
+    _dec2,
+    _class2;
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var Emit = function Emit(_ref) {
   var listeners = _ref.listeners;
-
-  var ls = listeners;
   return {
     emit: function emit(label) {
       for (var _len = arguments.length, args = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
         args[_key - 1] = arguments[_key];
       }
 
-      var listeners = ls[label];
+      listeners = listeners[label];
       if (listeners && listeners.length) {
         listeners.forEach(function (listener) {
           return listener.apply(undefined, args);
@@ -102,23 +106,20 @@ var Emit = function Emit(_ref) {
 
 var AddListener = function AddListener(_ref2) {
   var listeners = _ref2.listeners;
-
   return {
     addListener: function addListener(label, callback) {
       if (!listeners[label]) listeners[label] = [];
       listeners[label].push(callback);
     }
+
   };
 };
 
 var RemoveListener = function RemoveListener(_ref3) {
   var listeners = _ref3.listeners;
-
-  var ls = listeners;
   return {
     removeListener: function removeListener(label, observerName) {
       var index = null;
-      var listeners = ls;
       if (listeners[label] && listeners[label].length) {
         for (var i = 0, len = listeners[label].length; i < len; i++) {
           if (listeners[label][i].name.includes(observerName)) {
@@ -133,13 +134,12 @@ var RemoveListener = function RemoveListener(_ref3) {
         }
       }
     }
+
   };
 };
 
 var ChildNotify = function ChildNotify(_ref4) {
-  var emit = _ref4.emit;
-
-  var em = emit;
+  var _emit = _ref4.emit;
   return {
     emit: function emit(label) {
       for (var _len2 = arguments.length, args = Array(_len2 > 1 ? _len2 - 1 : 0), _key2 = 1; _key2 < _len2; _key2++) {
@@ -147,29 +147,24 @@ var ChildNotify = function ChildNotify(_ref4) {
       }
 
       // same as emit but fired from child to notify parent that notifies all children
-      return em.apply(undefined, [label].concat(args));
+      return _emit.apply(undefined, [label].concat(args));
     }
   };
 };
 
 var AttachListeners = function AttachListeners(_ref5) {
   var listeners = _ref5.listeners;
-
-  var ls = listeners || {};
   return {
-    listeners: ls
+    listeners: listeners || {}
   };
 };
 
 var Unsubscribe = function Unsubscribe(_ref6) {
   var parent = _ref6.parent,
       name = _ref6.name;
-
-  var pr = parent;
-  var nm = name;
   return {
     unsubscribe: function unsubscribe(label) {
-      pr.removeListener(label, nm);
+      parent.removeListener(label, name);
     }
   };
 };
@@ -177,24 +172,23 @@ var Unsubscribe = function Unsubscribe(_ref6) {
 var Subscribe = function Subscribe(_ref7) {
   var parent = _ref7.parent,
       name = _ref7.name;
-
-  var pr = parent;
-  var nm = name;
   return {
     subscribe: function subscribe(label, fn) {
       Object.defineProperty(fn, 'name', {
         enumerable: false,
         configurable: true,
         writable: true,
-        value: nm
+        value: name
       });
-      pr.addListener(label, fn.bind(_this));
+      parent.addListener(label, fn.bind(_this));
     }
   };
 };
 
 function createObservable() {
-  var listeners = {};
+  var parent = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : null;
+
+  var listeners = parent ? parent.listeners : {};
   return Object.assign({}, Emit({ listeners: listeners }), AddListener({ listeners: listeners }), RemoveListener({ listeners: listeners }), AttachListeners({ listeners: listeners }));
 }
 
@@ -203,7 +197,19 @@ function createObserver(name, parent) {
 }
 
 function makeObservable(parent) {
-  // TODO create decorator to make an observable
+  var attachListeners = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : true;
+
+  return function (Class) {
+    Object.assign(Class.prototype, createObservable(attachListeners ? parent : null));
+    return Class;
+  };
+}
+
+function makeObserver(name, parent) {
+  return function (Class) {
+    Object.assign(Class.prototype, createObserver(name, parent));
+    return Class;
+  };
 }
 
 var main = createObservable();
@@ -215,9 +221,31 @@ observer.subscribe('start', function (data) {
 observer2.subscribe('start', function (data) {
   return console.log(data);
 });
-observer.unsubscribe('start');
-main.emit('start', 'awsomeeeee');
-observer.emit('start', 'started in observer');
+// observer.unsubscribe('start')
+// main.emit('start', 'awsomeeeee')
+// observer.emit('start', 'started in observer')
+
+var Test = (_dec = makeObservable(main), _dec(_class = function Test() {
+  _classCallCheck(this, Test);
+}) || _class);
+
+
+var t = new Test();
+
+var observer3 = createObserver('d', t);
+observer3.subscribe('start', function (data) {
+  return console.log(data);
+});
+main.emit('start', 'started in main');
+var Obs = (_dec2 = makeObserver('dsasd', t), _dec2(_class2 = function Obs() {
+  _classCallCheck(this, Obs);
+}) || _class2);
+
+var obs = new Obs();
+obs.subscribe('start', function () {
+  return console.log('comes from obssss');
+});
+t.emit('start', 'started in decorated class');
 
 /***/ })
 /******/ ]);
--- test.js
+++ test.js
@@ -1,94 +1,73 @@
 
 
-const Emit = ({listeners}) =>{
-  let ls = listeners
-  return {
-    emit:(label, ...args)=>{
-      let listeners = ls[label]
-      if (listeners && listeners.length) {
-        listeners.forEach(listener => listener(...args))
-        return true
+const Emit = ({listeners}) =>({
+  emit:(label, ...args)=>{
+    listeners = listeners[label]
+    if (listeners && listeners.length) {
+      listeners.forEach(listener => listener(...args))
+      return true
+    }
+    return false
+  }
+})
+
+const AddListener = ({listeners}) =>({
+  addListener:(label, callback)=>{
+    if(!listeners[label]) listeners[label] = []
+    listeners[label].push(callback)
+  }
+
+})
+
+const RemoveListener = ({listeners}) =>({
+  removeListener:(label, observerName)=>{
+    let index = null
+    if(listeners[label] && listeners[label].length){
+      for(let i = 0, len = listeners[label].length; i < len; i++){
+        if(listeners[label][i].name.includes(observerName)){
+          index = i
+          break
+        }
       }
-      return false
-    }
-  }
-}
-
-const AddListener = ({listeners}) =>{
-  return {
-    addListener:(label, callback)=>{
-      if(!listeners[label]) listeners[label] = []
-      listeners[label].push(callback)
-    }
-  }
-}
-
-const RemoveListener = ({listeners}) =>{
-  let ls = listeners
-  return {
-    removeListener:(label, observerName)=>{
-      let index = null
-      let listeners = ls
-      if(listeners[label] && listeners[label].length){
-        for(let i = 0, len = listeners[label].length; i < len; i++){
-          if(listeners[label][i].name.includes(observerName)){
-            index = i
-            break
-          }
-        }
-        if(index !== null){
-          listeners[label] = listeners[label].filter(fn => fn !== listeners[label][index])
-        }
+      if(index !== null){
+        listeners[label] = listeners[label].filter(fn => fn !== listeners[label][index])
       }
     }
   }
-}
 
-const ChildNotify = ({emit}) =>{
-  let em = emit
-  return {
-    emit:(label, ...args)=>{
-      // same as emit but fired from child to notify parent that notifies all children
-      return em(label, ...args)
-    }
+})
+
+const ChildNotify = ({emit}) =>({
+  emit:(label, ...args)=>{
+    // same as emit but fired from child to notify parent that notifies all children
+    return emit(label, ...args)
   }
-}
+})
 
-const AttachListeners = ({listeners}) => {
-  let ls = listeners || {}
-  return {
-    listeners: ls
+const AttachListeners = ({listeners}) => ({
+  listeners:listeners || {}
+})
+
+const Unsubscribe = ({parent, name}) =>({
+  unsubscribe: label =>{
+    parent.removeListener(label, name)
   }
-}
+})
 
-const Unsubscribe = ({parent, name}) =>{
-  let pr = parent
-  let nm = name
-  return {
-    unsubscribe: label =>{
-      pr.removeListener(label, nm)
-    }
+const Subscribe = ({parent, name}) =>({
+  subscribe:(label, fn)=>{
+    Object.defineProperty(fn,'name',{
+      enumerable: false,
+      configurable: true,
+      writable: true,
+      value: name
+    })
+    parent.addListener(label, fn.bind(this))
   }
-}
+})
 
-const Subscribe = ({parent, name}) =>{
-  let pr = parent
-  let nm = name
-  return {
-    subscribe:(label, fn)=>{
-      Object.defineProperty(fn,'name',{
-        enumerable: false,
-        configurable: true,
-        writable: true,
-        value: nm
-      })
-      pr.addListener(label, fn.bind(this))
-    }
-  }
-}
-
-function createObservable () {
-  let listeners = {}
+function createObservable (parent = null) {
+  let listeners = parent ? parent.listeners : {}
   return Object.assign({},
     Emit({listeners}),
     AddListener({listeners}),
@@ -105,8 +84,18 @@ function createObserver(name, parent){
   )
 }
 
-function makeObservable(parent){
-  // TODO create decorator to make an observable
+function makeObservable(parent, attachListeners = true){
+  return function(Class){
+    Object.assign(Class.prototype, createObservable(attachListeners?parent:null))
+    return Class
+  }
+}
+
+function makeObserver(name, parent){
+  return function(Class){
+    Object.assign(Class.prototype, createObserver(name, parent))
+    return Class
+  }
 }
 
 
@@ -115,6 +104,28 @@ let observer = createObserver('observer1', main)
 let observer2 = createObserver('observer2', main)
 observer.subscribe('start',data=>console.log(data))
 observer2.subscribe('start',data=>console.log(data))
-observer.unsubscribe('start')
-main.emit('start', 'awsomeeeee')
-observer.emit('start', 'started in observer')
+// observer.unsubscribe('start')
+// main.emit('start', 'awsomeeeee')
+// observer.emit('start', 'started in observer')
+
+@makeObservable(main)
+class Test{
+  constructor(){
+
+  }
+}
+
+let t = new Test()
+
+let observer3 = createObserver('d', t)
+observer3.subscribe('start',data=>console.log(data))
+main.emit('start', 'started in main')
+@makeObserver('dsasd', t)
+class Obs{
+  constructor(){
+
+  }
+}
+let obs = new Obs()
+obs.subscribe('start',()=>console.log(`comes from obssss`))
+t.emit('start', 'started in decorated class')
